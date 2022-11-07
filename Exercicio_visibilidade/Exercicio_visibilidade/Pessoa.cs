3ae9641573c436b73f91a2c19b6c114f25567335
Assagra <117538517+Assagra@users.noreply.github.com>
2022-11-06 21:18:18 -0300

    Add files via upload

---
+++ Exercicio_visibilidade/Exercicio_visibilidade/Pessoa.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_visibilidade
+{
+    class Pessoa
+    {
+
+        private String nome;
+        private int idade;
+        private char sexo;
+
+        public void setNome(String nome)
+        {
+            this.nome = nome;
+        }
+
+        public String getNome()
+        {
+            return this.nome;
+        }
+
+        public void setIdade(int idade)
+        {
+            this.idade = idade;
+        }
+
+        public int getIdade()
+        {
+            return this.idade;
+        }
+
+        public void setSexo(char sexo)
+        {
+            this.sexo = sexo;
+        }
+
+        public char getSexo()
+        {
+            return this.sexo;
+        }
+
+        public Pessoa(string nome, int idade, char sexo)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.sexo = sexo;
+        }
+
+        public Pessoa()
+        {
+        }
+    }
+
+}
---
+++ Exercicio_visibilidade/Exercicio_visibilidade/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_visibilidade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Pessoa p1 = new Pessoa("ana", 23, 'F');
+            Pessoa p2 = new Pessoa("Paula", 35, 'M');
+            Pessoa p3 = new Pessoa();
+
+
+            //p1.setNome("Ana"); 
+            //p2.setNome("Paulo");
+            //p3.setNome("Lia");
+
+            //p1.setIdade(23);
+            //p2.setIdade(35);
+            //p3.setIdade(12);
+
+            p1.setSexo('F');
+            p2.setSexo('M');
+            p3.setSexo('F');
+
+            Console.WriteLine(p1.getNome() + " " + p1.getIdade() + " " + p1.getSexo());
+            Console.WriteLine(p2.getNome() + " " + p2.getIdade() + " " + p2.getSexo());
+            Console.WriteLine(p3.getNome() + " " + p3.getIdade() + " " + p3.getSexo());
+
+            Console.ReadKey();
+        }   
+    }
+}
