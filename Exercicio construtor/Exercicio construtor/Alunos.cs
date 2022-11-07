cc8fed94ee39f496268f0ad4101f757e58ed4803
Assagra <117538517+Assagra@users.noreply.github.com>
2022-11-06 21:10:40 -0300

    Add files via upload

---
+++ Exercicio construtor/Exercicio construtor/Alunos.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_construtor
+{
+    class Alunos
+    {
+        private string nome;
+        private int idade;
+        private string email;
+
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public void setIdade(int idade)
+        {
+            this.idade = idade;
+        }
+
+        public void setEmail(string email)
+        {
+            this.email = email;
+        }
+
+
+        public string getNome()
+        {
+            return this.nome;
+        }
+
+        public int getIdade()
+        {
+            return this.idade;
+        }
+
+        public string getEmail()
+        {
+            return this.email;
+        }
+
+
+        public Alunos(string nome, int idade, string email)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.email = email;
+        }
+
+        public Alunos(string nome, int idade)
+        {
+            this.nome = nome;
+            this.idade = idade;
+        }
+
+        public Alunos(string nome, string email)
+        {
+            this.nome = nome;
+            this.email = email;
+        }
+
+    }
+}
---
+++ Exercicio construtor/Exercicio construtor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_construtor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Alunos a1 = new Alunos("ze", 16, "ZeDoPneu@gmail.com");
+            Alunos a2 = new Alunos("Ana", "Aninha@terra.com.hd");
+            Alunos a3 = new Alunos("Paulo", 17);
+
+            a2.setIdade(15);
+            a3.setEmail("Paulo@ig.com.br");
+
+            Console.WriteLine(a1.getNome() + " " + a1.getIdade() + " " + a1.getEmail());
+            Console.WriteLine(a2.getNome() + " " + a2.getIdade() + " " + a2.getEmail());
+            Console.WriteLine(a3.getNome() + " " + a3.getIdade() + " " + a3.getEmail());
+
+  
+  
+        }
+    }
+}
